956e3faae6c36c7ea308a27e45f56847b9d3b757
johannes-kostas goetzinger <goetzinger@mediapeers.com>
2015-12-08 12:35:53 +0100

    removes obsolete files

--- dist/foo.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../typings/tsd.d.ts" />
-declare module Chinchilla {
-    class Foo {
-        bar(domain: string): string;
-    }
-}
--- dist/session.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../typings/uri-templates.d.ts" />
-/// <reference path="foo.d.ts" />
-declare module Chinchilla {
-    class Session {
-        domain: string;
-        opts: Object;
-        setSessionDomain(domain: string): void;
-    }
-}
