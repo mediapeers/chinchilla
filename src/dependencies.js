0463ca8c02bbe4f5930495713a8ceccdca2c77c0
johannes-kostas goetzinger <goetzinger@mediapeers.com>
2015-12-16 11:15:01 +0100

    updates dependencies package

--- src/dependencies.js
+++ src/dependencies.js
@@ -1,3 +1,5 @@
-window.Promise = require('bluebird');
-window.request = require('superagent');
-window.Cookies = require('js-cookie');
+window.Promise      = require('bluebird');
+window.request      = require('superagent');
+window.Cookies      = require('js-cookie');
+window._            = require('lodash');
+window.UriTemplate  = require('uri-templates');
